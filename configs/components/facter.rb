0c8ee0fc9c9f99daa7a7143fa17281aa2621bda7
Charlie Sharpsteen <source@sharpsteen.net>
2019-05-11 13:30:15 -0700

    Update facter to build on debian armhf
    This commit updates the facter component to build on the debian-9-armhf
    and debian-10-armhf platforms using Debian's cmake and to link against
    Debians's Boost and YAML-CPP libraries.

--- configs/components/facter.rb
+++ configs/components/facter.rb
@@ -121,6 +121,23 @@ component "facter" do |pkg, settings, platform|
     boost_static_flag = "-DBOOST_STATIC=OFF"
     special_flags += "-DCMAKE_CXX_FLAGS='#{settings[:cflags]}' -DENABLE_CXX_WERROR=OFF"
     yamlcpp_static_flag = "-DYAMLCPP_STATIC=OFF"
+  elsif platform.is_cross_compiled_linux? && platform.name =~ /debian-(?:9|10)/
+    # Use system cmake and libraries
+    cmake = '/usr/bin/cmake'
+
+    # Env variables used by CMakeCross.txt to select the desired
+    # crossbuild-essential-<arch> toolchain.
+    pkg.environment('DEB_HOST_ARCH', platform.architecture)
+    pkg.environment('DEB_HOST_GNU_TYPE', platform.platform_triple)
+    toolchain = '-DCMAKE_TOOLCHAIN_FILE=/etc/dpkg-cross/cmake/CMakeCross.txt'
+
+    boost_static_flag = "-DBOOST_STATIC=OFF"
+    yamlcpp_static_flag = "-DYAMLCPP_STATIC=OFF"
+    special_flags += ["-DBOOST_INCLUDEDIR=/usr/include",
+                      "-DBOOST_LIBRARYDIR=/usr/lib/#{platform.platform_triple}",
+                      "-DCMAKE_FIND_ROOT_PATH='/opt/puppetlabs/puppet;/usr'"].join(' ')
+
+    special_flags += ' -DENABLE_CXX_WERROR=OFF' if platform.name =~ /debian-10/
   elsif platform.is_cross_compiled_linux?
     ruby = "#{settings[:host_ruby]} -r#{settings[:datadir]}/doc/rbconfig-#{settings[:ruby_version]}-orig.rb"
     toolchain = "-DCMAKE_TOOLCHAIN_FILE=/opt/pl-build-tools/#{settings[:platform_triple]}/pl-build-toolchain.cmake"
